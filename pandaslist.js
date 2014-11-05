c4029a6ea4d333c228979193f976d28c54b6ecb8
Marc Novakowski <marc@pandora.com>
2014-11-05 01:06:44 -0800

    Adding google auth, restricted to pandora.com domain

---
+++ pandaslist.js
@@ -0,0 +1,25 @@
+if (Meteor.isClient) {
+  // client-specific code
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+
+  // Configure Google auth
+  // first, remove configuration entry in case service is already configured
+  ServiceConfiguration.configurations.remove({
+    service: "google"
+  });
+  ServiceConfiguration.configurations.insert({
+    service: "google",
+    clientId: "662229862340-8bjks10vu8grjqsabjml3f1qfvd3ltdg.apps.googleusercontent.com",
+    secret: "IGO8ODkE5FwprZfgrdZGHp35"
+  });
+}
+
+Accounts.config({
+  restrictCreationByEmailDomain: "pandora.com",
+  loginExpirationInDays: null
+});

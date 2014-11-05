7a33b7c64da5790caeeec79bb46dc9da9e89237f
Marc Novakowski <marc@pandora.com>
2014-11-05 13:50:30 -0800

    Image upload work in progress

--- client/templates/temp_form.js
+++ client/templates/temp_form.js
@@ -1,13 +1,30 @@
 Template.tempForm.events({
   'submit form': function(e) {
-	    e.preventDefault();
+		e.preventDefault();
 		
-	    var post = {
-	      title: $(e.target).find('[name=title]').val(),
-	      description: $(e.target).find('[name=description]').val(),
-	      category: $(e.target).find('[name=category]').val()
-	    };
+		var post = {
+		  title: $(e.target).find('[name=title]').val(),
+		  description: $(e.target).find('[name=description]').val(),
+		  category: $(e.target).find('[name=category]').val()
+		};
 
-	    Posts.insert(post);  
+		Posts.insert(post);  
+	},
+
+	'change .file_bag': function(event, template) {
+		FS.Utility.eachFile(event, function(file) {
+			Images.insert(file, function (err, fileObj) {
+			//Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+			console.log(err);
+			console.log(fileObj);
+			});
+		});
+	},
+
+	"click button.upload": function(e){
+		e.preventDefault();
+		var files = $("input.file_bag")[0].files
+
 	}
+
 });
--- pandaslist.js
+++ pandaslist.js
@@ -1,10 +1,10 @@
 if (Meteor.isClient) {
-  // client-specific code
-  Template.listings.helpers({
-    listing: function() {
-        return Posts.find();
-      }
-  });
+	// client-specific code
+	Template.listings.helpers({
+		listing: function() {
+				return Posts.find();
+			}
+	});
 }
 
 if (Meteor.isServer) {
@@ -22,9 +22,20 @@ if (Meteor.isServer) {
 		clientId: "662229862340-8bjks10vu8grjqsabjml3f1qfvd3ltdg.apps.googleusercontent.com",
 		secret: "IGO8ODkE5FwprZfgrdZGHp35"
 	});
+
+	// Configure S3
+	// S3.config = {
+	// 	key: 'AKIAIDYZL6G4P4STOEUA',
+	// 	secret: '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',
+	// 	bucket: 'pandaslist'
+	// };
 }
 
 Accounts.config({
 	restrictCreationByEmailDomain: "pandora.com",
 	loginExpirationInDays: null
+});
+
+Images = new FS.Collection("images", {
+  stores: [new FS.Store.FileSystem("images", {path: "public/uploads"})]
 });

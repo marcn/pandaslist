8416a9dfa95e66f686d6791e88712d63f2c38e16
Marc Novakowski <marc@pandora.com>
2014-11-05 15:32:29 -0800

    S3 is back in action - removing credentials from git

--- meteor/pandaslist.js
+++ meteor/pandaslist.js
@@ -22,20 +22,9 @@ if (Meteor.isServer) {
 		clientId: "662229862340-8bjks10vu8grjqsabjml3f1qfvd3ltdg.apps.googleusercontent.com",
 		secret: "IGO8ODkE5FwprZfgrdZGHp35"
 	});
-
-	// Configure S3
-	// S3.config = {
-	// 	key: 'AKIAIDYZL6G4P4STOEUA',
-	// 	secret: '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',
-	// 	bucket: 'pandaslist'
-	// };
 }
 
 Accounts.config({
 	restrictCreationByEmailDomain: "pandora.com",
 	loginExpirationInDays: null
-});
-
-Images = new FS.Collection("images", {
-  stores: [new FS.Store.FileSystem("images", {path: "public/uploads"})]
 });

8651be8761efc4ce4e2075e9a15336a7ad1e9009
Daniel Santiago-Rivera <dsantiagorivera@pandora.com>
2014-11-05 21:00:21 -0800

    We have DDP working back with Otto events

--- mobile/android/PandaList/app/src/main/java/com/pandora/hackathon/pandalist/PandaListApplication.java
+++ mobile/android/PandaList/app/src/main/java/com/pandora/hackathon/pandalist/PandaListApplication.java
@@ -3,8 +3,10 @@ package com.pandora.hackathon.pandalist;
 import android.app.Application;
 import android.content.Context;
 
+import com.pandora.hackathon.pandalist.activity.PandaListActivity;
 import com.pandora.hackathon.pandalist.ddp.MyDDPState;
 import com.pandora.hackathon.pandalist.gcm.GcmService;
+import com.squareup.otto.Bus;
 
 /**
  * Created by psingh on 11/5/14.
@@ -12,26 +14,35 @@ import com.pandora.hackathon.pandalist.gcm.GcmService;
 public class PandaListApplication extends Application {
 
     private static Context sContext = null;
+    private static Bus appBus = null;
+    private static MyDDPState ddp = null;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         PandaListApplication.sContext = getApplicationContext();
+        PandaListApplication.appBus = new Bus();
+        ddp = MyDDPState.setupInstance(sContext);
 
         // Perform init
         doInit();
+    }
+
+    private void doInit() {
+        //Setup GCMSservice
+        GcmService.init(getApplicationContext(), getString(R.string.gcm_sender_id));
     }
 
     public static Context getAppContext() {
         return PandaListApplication.sContext;
     }
 
-    private void doInit() {
-        // Setup DDM
-        MyDDPState.setupInstance(sContext);
+    public static Bus getBus() {
+        return appBus;
+    }
 
-        //Setup GCMSservice
-        GcmService.init(getApplicationContext(), getString(R.string.gcm_sender_id));
+    public static MyDDPState getDDP() {
+        return ddp;
     }
 }
---
+++ mobile/android/PandaList/app/src/main/java/com/pandora/hackathon/pandalist/events/DPPConnectEvent.java
@@ -0,0 +1,9 @@
+package com.pandora.hackathon.pandalist.events;
+
+/**
+ * Created by dsantiagorivera on 11/5/14.
+ */
+public class DPPConnectEvent {
+    public DPPConnectEvent() {
+    }
+}

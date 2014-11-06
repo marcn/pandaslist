3325ead39251bc68da2fc72412d0fcde22869d9c
Ryan <rmordoff@gmail.com>
2014-11-06 10:25:04 -0800

    add sub category nav

--- meteor/categories.js
+++ meteor/categories.js
@@ -1,4 +1,9 @@
 if (Meteor.isClient) {
+
+  var isSelected = function(category, subCategory, queryParams) {
+    return queryParams.subCategory === subCategory && queryParams.category === category;
+  };
+
   Template.categories.helpers({
     categoryGroups: function() {
       var posts = Posts.find().fetch();
@@ -8,14 +13,36 @@ if (Meteor.isClient) {
       // turn obj to arr
     	for (var key in groups) {
     		var obj = groups[key];
-        var selected = (this.query.category == key);
-    		arr.push({category: key, count: groups[key].length, selected: selected})
+    		arr.push({category: key, count: groups[key].length, params: this})
     	}
       return arr;
     }
   });
 
-  Template.subCategories.helpers({
+  Template.category.helpers({
+    subCategoryGroups: function() {
+      var posts = Posts.find({category: this.category}).fetch();
+      var groups = _.groupBy(_.pluck(posts, 'subCategory'));
+      
+      // turn obj to arr
+      var arr = [];
+      // add All subcategory
+      arr.push({category: this.category, subCategory: "All", count: posts.length, 
+        selected: isSelected(this.category, "All", this.params.query)});
+    
+      // add rest of subcateories
+      for (var key in groups) {
+        var obj = groups[key];
+        if(key !== "undefined")
+          arr.push({category: this.category, subCategory: key, count: groups[key].length, 
+            selected: isSelected(this.category, key, this.params.query)});
+      }
+
+      return arr;
+    }
+  });
+
+  Template.subCategory.helpers({
     cssClass: function() {
       if(this.selected)
         return "active";
@@ -25,7 +52,7 @@ if (Meteor.isClient) {
   });
 
 
-  Template.subCategories.events({
+  Template.subCategory.events({
   	"click li": function(evt) {
   		var path = $(evt.currentTarget).data('listing-category');
   		Router.go(path);

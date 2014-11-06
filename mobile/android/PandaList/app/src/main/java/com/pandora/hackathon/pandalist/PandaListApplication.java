51110b9e394f777d456fe8e29c22cac8990bb706
Ivan Johnson <pinheadhr@gmail.com>
2014-11-05 17:20:44 -0800

    GcmService
    Added gcm message service class outline.

--- mobile/android/PandaList/app/src/main/java/com/pandora/hackathon/pandalist/PandaListApplication.java
+++ mobile/android/PandaList/app/src/main/java/com/pandora/hackathon/pandalist/PandaListApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 
 import com.pandora.hackathon.pandalist.ddp.MyDDPState;
+import com.pandora.hackathon.pandalist.gcm.GcmService;
 
 /**
  * Created by psingh on 11/5/14.
@@ -29,5 +30,8 @@ public class PandaListApplication extends Application {
     private void doInit() {
         // Setup DDM
         MyDDPState.setupInstance(sContext);
+
+        //Setup GCMSservice
+        GcmService.init(getApplicationContext(), getString(R.string.gcm_sender_id));
     }
 }

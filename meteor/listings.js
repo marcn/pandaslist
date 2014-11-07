6e6566e38f0971fea45455e86c631b5264c4d157
Ryan <rmordoff@gmail.com>
2014-11-07 10:56:05 -0800

    finish up search

--- meteor/listings.js
+++ meteor/listings.js
@@ -12,7 +12,14 @@ if (Meteor.isClient) {
 				var reg = new RegExp(this.search,"gi");
 				filter = { $or: [{ title: reg }, { description: reg }, { location: reg } ] };
 			}
-			return Posts.find(filter).fetch();
+			var posts = Posts.find(filter).fetch();
+			var search = this.search;
+
+			// inject search term to post objects
+			return posts.map($.proxy(function(post) {
+			   post.search = this.search;
+			   return post;
+			}, this));				
 		}
 	});
 
@@ -21,7 +28,33 @@ if (Meteor.isClient) {
 			return (this.coverPhotoUrl) ? this.coverPhotoUrl : '/img/fpo.png';
 		},
 		isSearch: function() {
-			var asdf = "asdfa";
+			return (this.search) ? true : false;
+		},
+		truncatedDescription: function() {
+			return highlight(this.description, this.search);
+		},
+		highlightedTitle: function() {
+			return highlight(this.title, this.search);
 		}
 	});
+
+	var highlight = function(text, searchTerm) {
+    	var reg = new RegExp(searchTerm,"i");
+		var index = reg.exec(text).index;
+		var startIndex = index - 100;
+		if(startIndex < 0) {
+			startIndex = 0;
+		}
+		var endIndex = index + 100;
+		if(endIndex > text.length) {
+			endIndex = text.length;
+		}
+		
+		var truncatedDescription = "";
+		if(startIndex !== 0) { truncatedDescription = "..."; }
+		var truncatedDescription = text.substring(startIndex, endIndex);
+		if(endIndex !== text.length) { truncatedDescription += "..."; }
+
+		return truncatedDescription.replace(reg, "<span  class='search-term'>" + searchTerm + "</span>");
+  };
 }	
